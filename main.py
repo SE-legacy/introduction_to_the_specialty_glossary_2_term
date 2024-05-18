96b3b2d3b8e265c54d232def983edcb8a38ea312
ProstoLive <artem009saratov@gmail.com>
2024-05-18 21:33:21 +0400

    Fixed containment of files

---
+++ main.py
@@ -0,0 +1,21 @@
+import random
+
+words = []
+
+with open('words.txt', 'r', encoding='utf-8') as file_words:
+    for row in file_words:
+        words.append(row)
+
+print(*words, sep='\n')
+print(len(words))
+how_many_words = random.randint(55, 72)
+with open('result.txt', 'w') as f:
+    to_result = []
+    for i in range(0, how_many_words):
+        word_index = random.randint(0, len(words)-1)
+        word = words[word_index]
+        to_result.append(word)
+        words.pop(word_index)
+    words.sort()
+    for item in to_result:
+        f.write(item)
